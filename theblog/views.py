0d39e434a3802083f7201658bb5f7318b5235e56
Thomas Vu <thomas.vu@hotmail.com>
2023-08-27 20:45:08 +0200

    determine the correct user to edit posts

--- theblog/views.py
+++ theblog/views.py
@@ -12,8 +12,7 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     cats = Category.objects.all()
-    ordering = ['-post_date']
-    # ordering = ['-id']
+    ordering = ['-post_date', '-id']
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
